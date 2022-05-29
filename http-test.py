a0bcc56284e3b8ccd77d8007653c07f85026c1e2
Aki Sipovaara <mushut@gmail.com>
2022-05-29 09:26:30 +0300

    Starting to code the parser

--- http-test.py
+++ http-test.py
@@ -1,7 +1,28 @@
 import requests
+from html.parser import HTMLParser
 
-x = requests.get('https://barloose.com/')
+class HTML_parser(HTMLParser):
+	def handle_starttag(self, tag, attrs):
+		print("Encountered a start tag:", tag)
 
-print(x.text)
+	def handle_endtag(self, tag):
+		print("Encountered an end tag :", tag)
 
-# Ideana hakea skriptillä dataa verkkosivulta
+	def handle_data(self, data):
+		print("Encountered some data  :", data)
+
+def main():
+	bar_loose_html = requests.get('https://barloose.com/')
+	
+	# Print data for testing
+	# print(x.text)
+	
+	# parse html
+	parser = HTML_parser()
+	
+	# Testing parser
+	parser.feed(bar_loose_html)
+	
+	# search for tag that marks the event data
+
+main()
